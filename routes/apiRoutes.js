b3b23758faeb7a0ad7002b24ce08e9f6f8fa18ae
Wednesday <yensonyu@gmail.com>
2020-12-19 00:04:10 -0800

    create api routes js file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,12 @@
+var noteData = require("../db/db.json");
+
+module.exports = function(app) {
+    app.get("/api/notes", function(req, res) {
+        res.json(noteData);
+    });
+
+    app.post("/api/notes", function(req, res) {
+        noteData.push(req.body);
+    });
+
+}
