0421ac4ffe2c577f44bdbbc51a3d528827f9a926
Wednesday <yensonyu@gmail.com>
2020-12-20 00:30:33 -0800

    update access among files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,23 @@
-var noteData = require("../db/db.json");
+// var noteData = require("../db/db.json");
 var path = require('path');
 var fs = require('fs');
 
+var data = fs.readFileSync("db/db.json");
+var notesData = JSON.parse(data);
+
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
-        res.json(noteData);
+        res.json(notesData);
     });
 
     app.post("/api/notes", function(req, res) {
-        noteData.push(req.body);
+        notesData.push(req.body);
+        fs.writeFile("db/db.json", JSON.stringify(notesData));
+        res.json(noteData);
     });
+
+    // app.delete("/api/notes/:id", function(req, res) {
+
+    // });
 
 }
