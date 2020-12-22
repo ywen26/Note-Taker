df0356d034fbe01391a55eb28233b2e2503ed485
Wednesday <yensonyu@gmail.com>
2020-12-21 22:37:52 -0800

    add comment to html routes file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,14 @@
+// Dependencies
 var path = require('path');
 
+// HTML GET requests
 module.exports = function(app) {
+    // Basic route that sends the user with notes editing page
     app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
+    // Basic route that sends the user with start page
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     })
