08d56a5f80016fef4d745661b5f4677f532a1db4
Wednesday <yensonyu@gmail.com>
2020-12-21 16:44:55 -0800

    update delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,21 +18,38 @@ module.exports = function(app) {
     });
 
     // app.get("/api/notes/:id", function(req, res) {
-    //     var notesArray = [];
-    //     notesArray = notesData;
-    //     // var chosen = req.params.id;
-    //     // res.json(notesData[chosen]);
-    //     for (var i = 0; i < notesArray.length; i++) {
-    //         res.json(notsArray[i]);
+    //     var chosen = req.params.id;
+
+    //     // console.log(chosen);
+
+    //     for (var i = 0; i < notesData.length; i++) {
+    //         if (chosen === notesData[i].id) {
+    //             return res.json(notesData[i]);
+    //         }
     //     }
+    //     return res.json(false);
     // });
 
     app.delete("/api/notes/:id", function(req, res) {
+        // fs.readFile("./db/db.json", (err, allData) => {
+        //     if (err) throw err;
+
+        //     notesData = JSON.parse(allData);
+        //     var chosen = req.params.id;
+        //     notesData.splice(chosen, 1);
+    
+
+        //     fs.writeFileSync("db/db.json", JSON.stringify(notesData));
+        //     return res.json(true);
+        // });
         var chosen = req.params.id;
-        notesData.splice(chosen, 1);
+
+        notesData = notesData.filter(({ id }) => id != chosen);
+
+        // notesData.splice(chosen, 1);
         fs.writeFileSync("db/db.json", JSON.stringify(notesData));
-        console.log(chosen + "is Deleted.");
-        return res.json(chosen);
-        
+        // console.log(chosen + "is Deleted.");
+        return res.json(true);
+        // res.json(newNotesData);
     });
 }
