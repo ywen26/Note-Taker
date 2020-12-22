03df8a9c638c0079a75fb6c1923ab2482a44857b
Wednesday <yensonyu@gmail.com>
2020-12-21 22:32:25 -0800

    add comment to server.js file

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+// Dependencies
 var express = require('express');
 
+// Set up the Express App
 var app = express();
 var PORT = process.env.PORT || 3030;
 
+// Set up the Express App to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// Points the server to a series of "route" files
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+// Starts the server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
