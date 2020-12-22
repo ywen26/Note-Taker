89ca4f0c3723948b98e1fc7fc0888239afefef52
Wednesday <yensonyu@gmail.com>
2020-12-21 19:52:57 -0800

    adjustment to delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,3 @@
-// var path = require('path');
 var fs = require('fs');
 
 var data = fs.readFileSync("db/db.json");
@@ -17,39 +16,10 @@ module.exports = function(app) {
         return res.json(newNotes);
     });
 
-    // app.get("/api/notes/:id", function(req, res) {
-    //     var chosen = req.params.id;
-
-    //     // console.log(chosen);
-
-    //     for (var i = 0; i < notesData.length; i++) {
-    //         if (chosen === notesData[i].id) {
-    //             return res.json(notesData[i]);
-    //         }
-    //     }
-    //     return res.json(false);
-    // });
-
     app.delete("/api/notes/:id", function(req, res) {
-        // fs.readFile("./db/db.json", (err, allData) => {
-        //     if (err) throw err;
-
-        //     notesData = JSON.parse(allData);
-        //     var chosen = req.params.id;
-        //     notesData.splice(chosen, 1);
-    
-
-        //     fs.writeFileSync("db/db.json", JSON.stringify(notesData));
-        //     return res.json(true);
-        // });
         var chosen = req.params.id;
-
         notesData = notesData.filter(({ id }) => id != chosen);
-
-        // notesData.splice(chosen, 1);
         fs.writeFileSync("db/db.json", JSON.stringify(notesData));
-        // console.log(chosen + "is Deleted.");
         return res.json(true);
-        // res.json(newNotesData);
     });
 }
